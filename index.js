c48b7f3676a2e1adf4945e56ab54c51164d9b11c
Siddhartha Sahu <54734745+CallMeSahu@users.noreply.github.com>
2022-06-22 18:30:31 +0000

    feat: live class

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-//ex 02
-
-let strLength = (name, cb) =>{
-  cb(name, name.length)
-}
+//ex:2
 
 let printName = (name, len) => {
   console.log(`OMG ${name} is ${len} characters long!!`)
 }
 
+let strLength = (name, cb) =>{
+  cb(name, name.length)
+}
+
 strLength("Siddhartha", printName);
 strLength("Vidhu", printName);
 
-//ex 03
+//ex:3
 
 let success = () => console.log("Yay, I am alive!")
 let fail = () => console.log("Give my bose speakers and apple headphones to my sister")
@@ -27,4 +27,74 @@ let willThanosKillMe = (name, cb1, cb2) =>{
 
 willThanosKillMe("Siddhartha", success, fail);
 willThanosKillMe("Vidhu", success, fail);
+
+//ex:4
+
+const printAfterDelay = (message, delay) => setTimeout(() => console.log(message), delay);
+
+printAfterDelay("Hi Siddhartha", 2000);
+
+//ex:11
+function fakeFetch(msg, shouldReject) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldReject) {
+        reject(`error from server: ${msg}`)
+      }
+      resolve(`from server: ${msg}`)
+    }, 3000)
+  })
+}
+
+fakeFetch("Siddhartha is Awesome!")
+.then(data => console.log(data))
+
+//ex:12
+
+fakeFetch("Siddhartha is Awesome!")
+.then(data => console.log(data))
+.catch(error => console.log(error))
+
+//ex:13
+
+const getServerResponseLength = (msg) =>{
+  fakeFetch(msg)
+  .then(data => data.length)
+}
+
+getServerResponseLength("Siddhartha");
+
+//ex:14
+const syncCallsTogether = (msg1,msg2) =>{
+  fakeFetch(msg1)
+  .then(dataForMsg1 => fakeFetch(msg2)
+       .then(dataForMsg2 => console.log({dataForMsg1, dataForMsg2})))
+}
+
+syncCallsTogether("Siddhartha", "Sahu");
+
+const parallelCallsToServer = (msg1, msg2) => {
+  
+  fakeFetch(msg1).then(output1 => console.log({output1}));
+  fakeFetch(msg2).then(output2 => console.log({output2}));
+}
+
+parallelCallsToServer("Siddhartha", "Sahu")
+
+//ex:15
+const mainCall = async (msg) => {
+  const data = await fakeFetch(msg);
+  console.log(data)
+}
+
+mainCall("Hi Siddhartha Sahu")
+
+//ex:16
+const syncCallToServer2 = async (msg1, msg2) => {
+  const dataFromOne = await fakeFetch(msg1);
+  const dataFromTwo = await fakeFetch(msg2);
+  console.log({dataFromOne, dataFromTwo});
+}
+
+syncCallToServer2("Siddhartha", "Sahu");
 
